9c30055d748e8fa7091194a43da56f13f468d590
Sergey Zelman <sergeyzelman373@gmail.com>
2023-08-15 17:50:33 -0700

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+function About() {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8 ">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
+          exercitationem tenetur reiciendis, et praesentium deleniti ipsa ut,
+          nesciunt dolor voluptatem voluptatum. Vel hic maiores laboriosam,
+          debitis explicabo facilis aut ipsam. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Sequi iste dolorem minus iure
+          praesentium provident! Delectus amet, beatae illo excepturi et
+          molestias nisi ducimus distinctio magnam blanditiis quibusdam cumque
+          quas?
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+          consequatur quo, error corporis maxime excepturi officiis saepe, neque
+          atque repellendus illo in? Laborum voluptate ab culpa. Reiciendis esse
+          in adipisci?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
